c49e5cc51d40a37561a364708ad739b6903cd402
Gabriel Mandelli Cardoso <gabriel.mc@itaipuparquetec.org.br>
2025-02-04 10:01:59 -0300

    commit

--- codeforces/1862/f.cpp
+++ codeforces/1862/f.cpp
@@ -12,14 +12,33 @@ signed main (){
 
     while (t--)
     {
-        int w, f, n, curF = 0, curW = 0;
+        int w, f, n, sumS = 0;
 
         cin >> w >> f >> n;
 
         vector<int> monsters(n);
-        for (auto &x : monsters) cin >> x;
-
+        for (int i = 0; i < n; i++)
+        {
+            cin >> monsters[i];
+            sumS += monsters[i];
+        }
         
+        vector<bool> dp(sumS+1);
+        dp[0] = true;
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int water = sumS; water - monsters[i] >= 0; water--) dp[water] = dp[water] or dp[water - monsters[i]];
+        }
+
+        int ans = 1e10;
+        for (int i = 0; i <= sumS; i++)
+        {
+            if(dp[i]) ans = min(ans, max((w + i - 1) / w, (sumS - i + f - 1)/f));
+            
+        }
+        
+        cout << ans << endl;
     }
 
     return 0;
