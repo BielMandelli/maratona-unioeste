6009178f2d3450ed895764cc1841a8a775405326
Gabriel Mandelli <gabriel.mandellicardoso@gmail.com>
2025-03-17 22:17:34 -0300

    commit

--- usaco/bronze/sorting/kayaking.cpp
+++ usaco/bronze/sorting/kayaking.cpp
@@ -12,11 +12,22 @@ signed main()
     cin >> n;
     vector<int> k(n*2);
     for(auto &x : k) cin >> x;
-
     sort(k.begin(), k.end());
 
-    
-    
+    int ans = 1e9;
+    for (int i = 0; i < n*2; i++)
+    {   
+        for (int j = 0; j < n*2; j++)
+        {   
+            int cur = 0;
+            vector<int> temp;
+
+            for (int l = 0; l < n*2; l++) if(l != i && l != j) temp.push_back(k[l]);
+            for (int l = 0; l < (n*2)-2; l+=2) cur += temp[l+1] - temp[l];
+            
+            ans = min(ans, cur);
+        }
+    }
     
     cout << ans << endl;
     
