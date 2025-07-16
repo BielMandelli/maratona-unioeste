cd67dd23b82332f13b6e84f4841172a21b2ddf01
Gabriel Mandelli <gabriel.mc@itaipuparquetec.org.br>
2025-07-16 11:28:45 -0300

    feat: a

--- gym/misc/toledo-2025/f.cpp
+++ gym/misc/toledo-2025/f.cpp
@@ -7,19 +7,79 @@ using namespace std;
 #define all(a) a.begin(), a.end()
 #define rall(a) a.rbegin(), a.rend()
 #define ld long double
+const ld DINF = 1e18;
+const ld pi = acos(-1.0);
+const ld eps = 1e-9;
+
+bool eq(ld a, ld b) {
+	return abs(a - b) <= eps;
+}
+
+struct pt { // ponto
+	ld x, y;
+	pt(ld x_ = 0, ld y_ = 0) : x(x_), y(y_) {}
+	bool operator < (const pt p) const {
+		if (!eq(x, p.x)) return x < p.x;
+		if (!eq(y, p.y)) return y < p.y;
+		return 0;
+	}
+	bool operator == (const pt p) const {
+		return eq(x, p.x) and eq(y, p.y);
+	}
+	pt operator + (const pt p) const { return pt(x+p.x, y+p.y); }
+	pt operator - (const pt p) const { return pt(x-p.x, y-p.y); }
+	pt operator * (const ld c) const { return pt(x*c  , y*c  ); }
+	pt operator / (const ld c) const { return pt(x/c  , y/c  ); }
+	ld operator * (const pt p) const { return x*p.x + y*p.y; }
+	ld operator ^ (const pt p) const { return x*p.y - y*p.x; }
+	friend istream& operator >> (istream& in, pt& p) {
+		return in >> p.x >> p.y;
+	}
+};
+
+struct line { // reta
+	pt p, q;
+	line() {}
+	line(pt p_, pt q_) : p(p_), q(q_) {}
+	friend istream& operator >> (istream& in, line& r) {
+		return in >> r.p >> r.q;
+	}
+};
+
+ld sarea(pt p, pt q, pt r) { // area com sinal
+	return ((q-p)^(r-q))/2;
+}
+
+bool ccw(pt p, pt q, pt r) { // se p, q, r sao ccw
+	return sarea(p, q, r) > eps;
+}
+
+bool isinseg(pt p, line r) { // se p pertence ao seg de r
+	pt a = r.p - p, b = r.q - p;
+	return eq((a ^ b), 0) and (a * b) < eps;
+}
+
+bool interseg(line r, line s) { // se o seg de r intersecta o seg de s
+	if (isinseg(r.p, s) or isinseg(r.q, s)
+		or isinseg(s.p, r) or isinseg(s.q, r)) return 1;
+
+	return ccw(r.p, r.q, s.p) != ccw(r.p, r.q, s.q) and
+			ccw(s.p, s.q, r.p) != ccw(s.p, s.q, r.q);
+}
 
 const ld LINF = 1e18;
 const int MAX = 51;
 
-int n, m; // numero de vertices
-vector<pair<ld, int>> g[MAX]; // {vizinho, peso}
-ll d[1 << m][MAX]; // dp[mask][v] = arvore minima com o subconjunto mask de S e o vertice v
+int n, m; 
+vector<pair<int, ld>> g[MAX]; 
+vector<int> pc;
+ld d[1 << 10][MAX]; 
 
-ll steiner(const vector<int> &S) {
-	int k = S.size(); // k--;
+ll steiner() {
+	int k = pc.size(); // k--;
 	for (int mask = 0; mask < (1 << k); mask++) for(int v = 0; v < n; v++) d[mask][v] = LINF;
 	for (int v = 0; v < n; v++) d[0][v] = 0;
-	for (int i = 0; i < k; ++i) d[1 << i][S[i]] = 0;
+	for (int i = 0; i < k; ++i) d[1 << i][pc[i]] = 0;
 	for (int mask = 1; mask < (1 << k); mask++) {
 		for (int a = (mask - 1) & mask; a; a = (a - 1) & mask) {
 			int b = mask ^ a;
@@ -27,7 +87,7 @@ ll steiner(const vector<int> &S) {
 			for (int v = 0; v < n; v++)
 				d[mask][v] = min(d[mask][v], d[a][v] + d[b][v]);
 		}
-		priority_queue<pair<ll, int>> pq;
+		priority_queue<pair<ld, int>> pq;
 		for (int v = 0; v < n; v++) {
 			if (d[mask][v] == LINF) continue;
 			pq.emplace(-d[mask][v], v);
@@ -41,13 +101,67 @@ ll steiner(const vector<int> &S) {
 			}
 		}
 	}
-	return d[(1 << k) - 1][S[0]]; // S[k]
+
+	return d[(1 << k) - 1][pc[0]]; // S[k]
 }
 
 void solve(){
     cin >> n >> m;
+	
+    vector<pt> c(n);
+	for (int i = 0; i < n; i++) {
+    	cin >> c[i];
+	}
+	
+	vector<line> wll(m);
+	for (int i = 0; i < m; i++) {
+    	cin >> wll[i];
+	}
 
-    
+	int s;
+	cin >> s;
+	for (int i = 0; i < s; i++) {
+		int x;
+		cin >> x;
+		pc.push_back(x-1);
+	}
+
+	for (int i = 0; i < n; ++i) {
+		for (int j = i + 1; j < n; ++j) {
+			line cable = {c[i], c[j]};
+			bool valid = true;
+
+			for (auto w : wll) {
+				if (interseg(cable, w)) {
+					valid = false;
+					break;
+				}
+			}
+
+			if (valid) {
+				for (int k = 0; k < n; ++k) {
+					if (k == i || k == j) continue;
+					if (isinseg(c[k], cable)) {
+						valid = false;
+						break;
+					}
+				}
+			}
+
+			if (valid) {
+				ld dist = hypot(c[i].x - c[j].x, c[i].y - c[j].y);
+				g[i].emplace_back(j, dist);
+				g[j].emplace_back(i, dist);
+			}
+		}
+	}
+
+	ld ans = steiner();
+	if (ans >= LINF)
+		cout << "impossivel\n";
+	else
+		cout << fixed << setprecision(3) << ans << '\n';
+
 }
 
 signed main(){
